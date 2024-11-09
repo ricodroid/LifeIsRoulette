46543f49778a8ddd97f0c1942e4ede1a6e6d488d
ricodroid404 <ricodroid2024@gmail.com>
2024-11-09 20:53:03 +0900

    バージョン更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,9 +12,9 @@ android {
     defaultConfig {
         applicationId = "com.ricodroid.roulettelife"
         minSdk = 29
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        targetSdk = 34
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
